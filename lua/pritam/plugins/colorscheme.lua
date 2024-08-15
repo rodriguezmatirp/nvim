21ade2141ce4c9498fd90063a48b38ee3da79831
rodriguezmatirp <tarunpritamvrs@gmail.com>
2024-08-16 00:53:23 +0530

    add: twlilight, modicator

--- lua/pritam/plugins/colorscheme.lua
+++ lua/pritam/plugins/colorscheme.lua
@@ -42,4 +42,15 @@ return {
 			vim.cmd([[colorscheme gruvbox]])
 		end,
 	},
+	{
+		"mawkler/modicator.nvim",
+		dependencies = "ellisonleao/gruvbox.nvim", -- Add your colorscheme plugin here
+		init = function()
+			-- These are required for Modicator to work
+			vim.o.cursorline = true
+			vim.o.number = true
+			vim.o.termguicolors = true
+		end,
+		opts = {},
+	},
 }
